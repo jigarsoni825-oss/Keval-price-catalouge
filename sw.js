f475dc306f820782640264fb3cc78cd02d62cf49
jigarsoni825-oss <jigar.soni825@gmail.com>
2026-05-11 00:17:00 +0530

    Add caching for assets in service worker
    Implement caching for assets during service worker installation.

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const CACHE_NAME = 'keval-v2';
+const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
