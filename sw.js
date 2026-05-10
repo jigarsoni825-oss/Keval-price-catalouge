6343adf0419fc3d8003e090c51b1c8c4bf3e8662
jigarsoni825-oss <jigar.soni825@gmail.com>
2026-05-11 00:33:55 +0530

    Update service worker cache name to 'keval-v3'

--- sw.js
+++ sw.js
@@ -1,10 +1,4 @@
-const CACHE_NAME = 'keval-v2';
+const CACHE = 'keval-v3';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
